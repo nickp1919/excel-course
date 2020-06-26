6ce1431a6c5e77bb3d771cad2a842213bb14c784
nickp1919 <64270163+nickp1919@users.noreply.github.com>
2020-06-26 17:19:48 +0300

    Framw-start V3 26.06.2020

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,15 +1,29 @@
+import {$} from '@core/dom'
+
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
+  getRoot() {
+
+    const $root = $.create('div', 'excel')
+
+    this.components = this.components.map(Component => {
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      $el.html(component.toHTML())
+      $root.append($el)
+      return component
+    })
+    console.log(this.components)
+    return $root
+  }
+
   render() {
-    // eslint-disable-next-line no-tabs,no-mixed-spaces-and-tabs
-  	// this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
-		 const node = document.createElement('h1')
-		 node.textContent = 'TEST'
-		 this.$el.append(node)
+    this.$el.append(this.getRoot())
+    this.components.forEach(component => component.init())
   }
 
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,4 +1,31 @@
+import {capitalize} from '@core/utils'
+
 export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provided for DomListener`)
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
 
+  initDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        throw new Error(
+            `Method ${method} is not implemented`
+        )
+      }
+      this.$root.on(listener, this[method].bind(this))
+    })
+  }
 
+  removeDomListeners() {
+    // realize
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
