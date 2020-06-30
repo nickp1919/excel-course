e5a53a5f0abf62526e8ada1f23909f697c59625b
nickp1919 <64270163+nickp1919@users.noreply.github.com>
2020-06-30 14:21:48 +0300

    Finished framework

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,24 +3,24 @@ const CODES = {
   Z: 90
 }
 
-// function createCell() {
-// 	return `
-// 	<div class="cell" contenteditable >1</div>
-// 	`
-// }
+function toCell() {
+  return `
+	<div class="cell" contenteditable ></div>
+	`
+}
 
-function toColum() {
+function toColum(col) {
   return `
 		<div class="column">
-      A
+      ${col}
   	</div>
 	`
 }
 
-function createRow(content) {
+function createRow(content, numberRow) {
   return `
 		<div class="row">
-			<div class="row-info">1</div>
+			<div class="row-info">${numberRow}</div>
 			<div class="row-data">${content}</div>
 		</div>
 	`
@@ -30,17 +30,23 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
+export function createTable(rowsCount = 20) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
       .map(toColum)
+      .join('')
 
-  rows.push(createRow(cols))
+  rows.push(createRow(cols, ''))
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
+  	const cells = new Array(colsCount)
+			  .fill('')
+			  .map(toCell)
+			  .join('')
+
+    rows.push(createRow(cells, i+1))
   }
 
   return rows.join('')
