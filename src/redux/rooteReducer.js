7e965f3c771ffb9ccf54ef66e5c8a4b6eca527dc
nickp1919 <64270163+nickp1919@users.noreply.github.com>
2020-08-11 09:31:42 +0300

    Routing implemented

--- src/redux/rooteReducer.js
+++ src/redux/rooteReducer.js
@@ -2,7 +2,7 @@ import {
   APPLY_STYLE,
   CHANGE_STYLES,
   CHANGE_TEXT, CHANGE_TITLE,
-  TABLE_RESIZE
+  TABLE_RESIZE, UPDATE_DATE
 } from '@/redux/types'
 
 
@@ -41,6 +41,8 @@ export function rootReducer(state, action) {
       }
     case CHANGE_TITLE:
       return {...state, title: action.data}
+    case UPDATE_DATE:
+      return {...state, openedDate: new Date().toJSON()}
 
     default: return state
   }
